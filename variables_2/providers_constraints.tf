c4c9aab4f8d1a64e1f5865ae8c81cf020d3f5391
Rober160 <145595171+Rober160@users.noreply.github.com>
2025-05-13 08:56:44 +0200

    Create providers_constraints.tf

---
+++ variables_2/providers_constraints.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=4.43.0, <4.47.0, !=4.43.0" 
+        //terraform init -upgrade
+        //tree .terraform
+    }
+  }
+  required_version = "1.11.4"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
