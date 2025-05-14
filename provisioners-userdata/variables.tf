10cfcd5b82578dffc8f6f62bc9e1b61cd3d2c717
Rober160 <145595171+Rober160@users.noreply.github.com>
2025-05-14 09:53:48 +0200

    Create variables.tf

---
+++ provisioners-userdata/variables.tf
@@ -0,0 +1,25 @@
+variable "virginia_cidr" {
+  description = "CIDR virginia"
+  type        = string
+  sensitive   = false
+}
+
+variable "subnets" {
+  description = "Lista de subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags del proyecto"
+  type        = map(string)
+}
+
+variable "sg_ingress_cidr" {
+  description = "CIDR para el tráfico de entrada"
+  type = string
+}
+
+variable "ec2_specs" {
+  description = "Parámetros de la instancia"
+  type = map(string)
+}
